a525b3cc90df363ead66f3a0212bf94f088f5486
loverthehater <2858172-loverthehater@users.noreply.github.com>
2018-12-14 02:10:35 -0600

    updated site, sampledb. now using scss.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,48 @@
 
 window.onload = () => {
-    const accordians = <HTMLCollectionOf<HTMLButtonElement>>document.getElementsByClassName('btn');
-    for (let i = 0; i < accordians.length; i += 1) {
-        accordians[i].onclick = (ev) => {
-            accordians[i].classList.toggle('active');
-            const panel = <HTMLElement>accordians[i].nextSibling;
-            console.log(panel);
-            if (panel.style.maxHeight) panel.style.maxHeight = `${panel.scrollHeight}px`;
-            else panel.style.maxHeight = null;
+    const expandables = document.getElementsByClassName('expandable');
+    for (const expandable of expandables) {
+        console.log('found expandable');
+        if (!(expandable instanceof HTMLElement)) return;
+        console.log('expandable is div');
+        const contents = expandable.nextElementSibling;
+        if (!(contents instanceof HTMLElement)) return;
+        console.log('contents are div');
+        expandable.onclick = () => {
+            expandable.classList.toggle('active');
+            if (!Number.parseInt(contents.style.maxHeight, 10)) {
+                contents.style.maxHeight = `${contents.scrollHeight}px`;
+            } else {
+                contents.style.maxHeight = '0';
+            }
         };
     }
 };
+
+/*
+        <h1 class="subject clickable expandable">Accounting</h1>
+        <div class="subject contents">
+            <h2 class="course clickable expandable">1100</h2>
+            <div class="course contents">
+                <h3 class="campus clickable expandable">Elkhorn Valley</h3>
+                <div class="campus contents">
+                    <h3 class="section clickable expandable">1A</h3>
+                    <div class="section contents">
+                        <p class="book clickable">Example Book Title: The Example</p>
+                        <div class="book contents">
+                        </div>
+                    </div>
+                    <div class="section">
+                        <div class="book"></div>
+                        <div class="book"></div>
+                    </div>
+                </div>
+                <div class="campus">
+                    <div class="section"></div>
+                    <div class="section">
+                        <div class="book"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+*/
